821f7b9dd8f41c32e3959021c3d1e686ca4642ed
zzc <wei970806@163.>
2020-09-24 16:42:26 +0800

    modify print info

--- src/s1_5/UFTest.java
+++ src/s1_5/UFTest.java
@@ -25,20 +25,36 @@ public class UFTest {
             int q = NumberUtils.toInt(nums[1]);
             uf.union(p, q);
         }
-        System.out.println(fileName + "文件耗时" + timer.getTime() + "秒");
+        System.out.println(uf.getClass().getSimpleName() + "算法    " + fileName + "文件耗时" + timer.getTime() + "秒");
         System.out.println(uf.count());
         br.close();
     }
 
     public static void main(String[] args) throws IOException {
-//        uf(new UnionFind1(10), "src/s1_5/tinyUF.txt");
-//        uf(new UnionFind1(625), "src/s1_5/mediumUF.txt");
-////        uf(new UnionFind1(1000000), "src/s1_5/largeUF.txt");
-//
-//        System.out.println("=========================================");
-//
+        uf(new UnionFind1(10), "src/s1_5/tinyUF.txt");
+        uf(new UnionFind1(625), "src/s1_5/mediumUF.txt");
+        uf(new UnionFind1(1000000), "src/s1_5/largeUF.txt");
+
+        System.out.println("=========================================");
+
         uf(new UnionFind2(10), "src/s1_5/tinyUF.txt");
         uf(new UnionFind2(625), "src/s1_5/mediumUF.txt");
         uf(new UnionFind2(1000000), "src/s1_5/largeUF.txt");
+
+        /**
+         * UnionFind1算法    src/s1_5/tinyUF.txt文件耗时0.0052606秒
+         * 2
+         * UnionFind1算法    src/s1_5/mediumUF.txt文件耗时0.0132184秒
+         * 3
+         * UnionFind1算法    src/s1_5/largeUF.txt文件耗时687.1904544秒
+         * 6
+         * =========================================
+         * UnionFind2算法    src/s1_5/tinyUF.txt文件耗时1.05E-4秒
+         * 2
+         * UnionFind2算法    src/s1_5/mediumUF.txt文件耗时4.953E-4秒
+         * 3
+         * UnionFind2算法    src/s1_5/largeUF.txt文件耗时0.8202628秒
+         * 6
+         */
     }
 }
